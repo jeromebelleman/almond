abeb59d644e13495ab4cb3d883d13ec96d681790
Jerome Belleman <jerome.belleman@gmail.com>
2017-05-14 20:01:13 +0200

    Improve setup.py and add doc

---
+++ README.md
@@ -0,0 +1,13 @@
+# NAME
+
+almond – Edit JSON as YAML
+
+# SYNOPSIS
+
+almond [--help]
+
+# DESCRIPTION
+
+Conveniently edit JSON via a YAML file opened in a bespoke Vim buffer. This
+stemmed from a need to edit multi-line strings in JSON data. I guess I could
+use something like that in [kibanda](http://cern.ch/jbl/tools/kibanda).
--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
+# coding=utf-8
 
+import os
 from distutils.core import setup
 
-setup(name='almond',
-      scripts=['almond'],
-     )
+delattr(os, 'link')
+
+setup(
+    name='almond',
+    version='1.0',
+    author='Jerome Belleman',
+    author_email='Jerome.Belleman@gmail.com',
+    url='http://cern.ch/jbl',
+    description="Edit JSON as YAML",
+    long_description="Edit JSON as YAML in Vim.",
+    scripts=['almond'],
+    data_files=[('share/man/man1', ['almond.1'])],
+)
